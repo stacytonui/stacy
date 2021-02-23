e411ca26aa3da1791da81a60d6a1e136d52cf63a
stacytonui <stacyanne01@gmail.com>
2021-02-23 23:37:32 +0300

    Updates

---
+++ precache-manifest.b464d8c17a5cd7867f0701fb7738b0ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "128fae676a800fa4bfdd097407ce9894",
+    "url": "/stacy/index.html"
+  },
+  {
+    "revision": "ba30c74b5f97afc9fc30",
+    "url": "/stacy/static/css/main.9c8d1904.chunk.css"
+  },
+  {
+    "revision": "c447b984f76aa10d29ad",
+    "url": "/stacy/static/js/2.85e564d4.chunk.js"
+  },
+  {
+    "revision": "ba30c74b5f97afc9fc30",
+    "url": "/stacy/static/js/main.5df1396f.chunk.js"
+  },
+  {
+    "revision": "8e6a57dde1ad34b64eb9",
+    "url": "/stacy/static/js/runtime~main.140d7ad5.js"
+  }
+]);
